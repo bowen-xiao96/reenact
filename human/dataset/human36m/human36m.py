rgbd normalization fixed

human/dataset/human36m/human36m.py
@@ -8,8 +8,8 @@ from PIL import Image
 import torch
 from torch.utils.data import *
 
-import dataset.human36m.skeleton as skeleton
-from .dataset_utils import *
+from . import skeleton  # import skeleton
+from .dataset_utils import *  # from dataset_utils import *
 
 
 class Human36m(Dataset):
@@ -21,9 +21,9 @@ class Human36m(Dataset):
         self.return_count = return_count
         self.image_size = image_size
         self.normalize = normalize
+
         self.keypoint_size = keypoint_size
         self.skeleton_width = skeleton_width
-
         self.extend_ratio = extend_ratio
         self.random_flip = random_flip
         self.random_crop = random_crop
@@ -32,44 +32,43 @@ class Human36m(Dataset):
         skeleton.skeleton_width = skeleton_width
 
         # load the list of all images
-        clip_list = os.listdir(root_dir)
-        self.clip_list = clip_list
-        self.clip_count = len(clip_list)
+        self.all_videos = os.listdir(root_dir)
+        self.video_count = len(self.all_videos)
 
-        self.pos = [0 for _ in range(self.clip_count)]
+        # current sample pos for each video
+        self.pos = [0 for _ in range(self.video_count)]
 
-        video_list = list()
-        for clip in clip_list:
-            with open(os.path.join(root_dir, clip, 'metadata.pkl'), 'rb') as f_in:
-                metadata_list = pickle.load(f_in)
+        self.all_metadata = list()
+        for video in self.all_videos:
+            with open(os.path.join(root_dir, video, 'metadata.pkl'), 'rb') as f_in:
+                metadata = pickle.load(f_in)
 
-            random.shuffle(metadata_list)
-            video_list.append(metadata_list)
-
-        self.video_list = video_list
+            random.shuffle(metadata)
+            self.all_metadata.append(metadata)
 
     def __len__(self):
-        return self.clip_count
+        return self.video_count
 
     def __getitem__(self, item):
-        clip = self.clip_list[item]
-        metadata_list = self.video_list[item]
-        image_count = len(metadata_list)
+        video = self.all_videos[item]
+        metadata = self.all_metadata[item]
+        image_count = len(metadata)
 
+        # self.pos[item], self.pos[item] + 1, ..., self.pos[item] + self.return_count - 1
         if self.pos[item] + self.return_count > image_count:
             # reset
             self.pos[item] = 0
-            random.shuffle(metadata_list)
+            random.shuffle(metadata)
 
         images = list()
         skeletons = list()
 
         # read images
         for i in range(self.return_count):
-            # [pos, pos + 1, ... pos + return_count - 1]
-            file_name, bbox, keypoints_img, depth = metadata_list[self.pos[item] + i]
+            idx = self.pos[item] + i
+            file_name, bbox, keypoints_img, depth = metadata[idx]
 
-            img_fullname = os.path.join(self.root_dir, clip, file_name)
+            img_fullname = os.path.join(self.root_dir, video, file_name)
             img = Image.open(img_fullname).convert('RGB')
             w, h = img.size
 
@@ -95,29 +94,27 @@ class Human36m(Dataset):
                 extend_up = random.uniform(max(0, extend - down), min(up, extend))
                 extend_down = extend - extend_up
 
-                x1 -= extend_left
-                y1 -= extend_up
-                x2 += extend_right
-                y2 += extend_down
-                side += extend
+                new_x1 = x1 - extend_left
+                new_y1 = y1 - extend_up
+                new_x2 = x2 + extend_right
+                new_y2 = y2 + extend_down
 
             else:
                 extend_ratio = float(self.extend_ratio) / 2  # for one side
                 extend = min(left, right, up, down)
                 extend = min(extend, side * extend_ratio)
 
-                x1 -= extend
-                y1 -= extend
-                x2 += extend
-                y2 += extend
-                side += 2 * extend
+                new_x1 = x1 - extend
+                new_y1 = y1 - extend
+                new_x2 = x2 + extend
+                new_y2 = y2 + extend
 
             # calculate new coordinates
-            keypoints_img[:, 0] -= x1
-            keypoints_img[:, 1] -= y1
+            keypoints_img[:, 0] -= new_x1
+            keypoints_img[:, 1] -= new_y1
 
             # crop the image
-            cropped_img = img.crop((x1, y1, x2, y2))
+            cropped_img = img.crop((new_x1, new_y1, new_x2, new_y2))
             new_w, new_h = cropped_img.size
 
             # random flip
@@ -148,12 +145,12 @@ class Human36m(Dataset):
 
                 skeletons.append(skeleton_img)
             elif self.modality == 'skeleton_rgbd':
-                skeleton_img, depth_map = skeleton.skeleton_rgbd(output_size, keypoints_img, depth, flip)
+                skeleton_img, depth_img = skeleton.skeleton_rgbd(output_size, keypoints_img, depth, flip)
                 skeleton_img = to_tensor(skeleton_img, self.normalize)
 
-                depth_map = torch.from_numpy(depth_map)
-                depth_map = torch.unsqueeze(depth_map, dim=0)
-                skeleton_img = torch.cat((skeleton_img, depth_map), dim=0)
+                depth_img = torch.from_numpy(depth_img)
+                depth_img = torch.unsqueeze(depth_img, dim=0)
+                skeleton_img = torch.cat((skeleton_img, depth_img), dim=0)
 
                 skeletons.append(skeleton_img)
             else:
@@ -162,7 +159,7 @@ class Human36m(Dataset):
         # move forward pointer
         self.pos[item] += self.return_count
 
-        # form output
+        # output
         skeleton_t = skeletons[0]
         image_t = images[0]
         skeletons = torch.stack(skeletons[1:])
@@ -176,7 +173,7 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     image_dir = r'D:\Work\human36m\sampled_images'
-    modality = 'skeleton_2d'
+    modality = 'skeleton_rgbd'
     return_count = 3
     image_size = 256
     normalize = True
@@ -192,32 +189,49 @@ if __name__ == '__main__':
 
     print(len(dataset))
 
-    for i in range(5):
-        for j, (item, images, skeletons, image_t, skeleton_t) in enumerate(dataset):
-            assert j == item
-            print('%d: %d' % (i, j))
+    for i, (item, images, skeletons, image_t, skeleton_t) in enumerate(dataset):
+        assert i == item
 
-            assert images.size(0) == return_count
-            assert skeletons.size(0) == return_count
-            images = torch.cat((torch.unsqueeze(image_t, dim=0), images), dim=0)
-            skeletons = torch.cat((torch.unsqueeze(skeleton_t, dim=0), skeletons), dim=0)
+        assert images.size(0) == return_count
+        assert skeletons.size(0) == return_count
 
-            output_img = Image.new('RGB', (return_all * image_size, 3 * image_size), 'white')
-            for t in range(return_all):
-                ii = to_pil_image(images[t], normalize)
-                output_img.paste(ii, (t * image_size, 0))
+        image_t = torch.unsqueeze(image_t, dim=0)
+        images = torch.cat((image_t, images), dim=0)
+        skeleton_t = torch.unsqueeze(skeleton_t, dim=0)
+        skeletons = torch.cat((skeleton_t, skeletons), dim=0)
 
-                ss = to_pil_image(skeletons[t], normalize)
-                output_img.paste(ss, (t * image_size, image_size))
+        # image
+        skeletons_rgb = skeletons[:, :-1, ...]
+        canvas = Image.new('RGB', (return_all * image_size, 3 * image_size), 'white')
+        for j in range(return_all):
+            # original RGB image
+            ii = to_pil_image(images[j], normalize)
+            canvas.paste(ii, (j * image_size, 0))
 
-                ii_arr = np.array(ii)
-                ss_arr = np.array(ss)
-                nonzero_pos = np.nonzero(np.sum(ss_arr, axis=-1))
-                fused = ii_arr.copy()
-                fused[nonzero_pos] = ss_arr[nonzero_pos]
-                fused = Image.fromarray(fused)
-                output_img.paste(fused, (t * image_size, 2 * image_size))
+            # RGB skeleton
+            ss = to_pil_image(skeletons_rgb[j], normalize)
+            canvas.paste(ss, (j * image_size, image_size))
 
-            plt.clf()
-            plt.imshow(output_img)
-            plt.show()
+            # skeleton drawn over the original image
+            ii_arr = np.array(ii)
+            ss_arr = np.array(ss)
+            nonzero_pos = np.nonzero(np.sum(ss_arr, axis=-1))
+            fused_arr = ii_arr.copy()
+            fused_arr[nonzero_pos] = ss_arr[nonzero_pos]
+            fused = Image.fromarray(fused_arr)
+            canvas.paste(fused, (j * image_size, 2 * image_size))
+
+        plt.clf()
+        plt.imshow(canvas)
+        plt.show()
+
+        # depth
+        skeletons_depth = skeletons[:, -1, ...]
+        depth_canvas = np.zeros((image_size, return_all * image_size), dtype=np.float32)
+        for j in range(return_all):
+            depth_canvas[..., j * image_size: (j + 1) * image_size] = skeletons_depth[j].numpy()
+
+        plt.clf()
+        plt.imshow(depth_canvas, cmap=skeleton.skeleton_colormap)
+        plt.colorbar()
+        plt.show()

human/dataset/human36m/skeleton.py
@@ -2,7 +2,7 @@ import os, sys
 import numpy as np
 
 import matplotlib.cm as cm
-import matplotlib.colors as colors
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
 from PIL import Image, ImageDraw
@@ -27,16 +27,17 @@ connection_count = len(connections)
 connections_flipped = list()
 
 for i, j in connections:
-    if keypoint_names[i].startswith('L_') or keypoint_names[j].startswith('L_'):
-        new_i = keypoint_names.index(keypoint_names[i].replace('L_', 'R_'))
-        new_j = keypoint_names.index(keypoint_names[j].replace('L_', 'R_'))
-    elif keypoint_names[i].startswith('R_') or keypoint_names[j].startswith('R_'):
-        new_i = keypoint_names.index(keypoint_names[i].replace('R_', 'L_'))
-        new_j = keypoint_names.index(keypoint_names[j].replace('R_', 'L_'))
-    else:
-        new_i, new_j = i, j
+    if keypoint_names[i].startswith('L_'):
+        i = keypoint_names.index(keypoint_names[i].replace('L_', 'R_'))
+    elif keypoint_names[i].startswith('R'):
+        i = keypoint_names.index(keypoint_names[i].replace('R_', 'L_'))
 
-    connections_flipped.append({new_i, new_j})
+    if keypoint_names[j].startswith('L_'):
+        j = keypoint_names.index(keypoint_names[j].replace('L_', 'R_'))
+    elif keypoint_names[j].startswith('R'):
+        j = keypoint_names.index(keypoint_names[j].replace('R_', 'L_'))
+
+    connections_flipped.append({i, j})
 
 # color of keypoints
 keypoint_2d_color = 'gray'
@@ -47,24 +48,17 @@ skeleton_colormap = cm.get_cmap(skeleton_colormap_name)
 skeleton_colors = list()
 
 for i in range(connection_count):
-    key = float(i) / (connection_count - 1)  # RGBA in [0, 1] range
-    skeleton_colors.append(skeleton_colormap(key))
+    key = float(i) / (connection_count - 1)  # [0, 1]
+    skeleton_colors.append(skeleton_colormap(key))  # RGBA in [0, 1] range
 
-depth_color_map = 'rainbow'
-
-depth_inf = 2.0
+DEPTH_INF = np.finfo(np.float32).max
 
 # keypoint and skeleton size
 keypoint_size = 10
 skeleton_width = 5
 
 
-def _depth_normalize_1(depth):
-    normalized_depth = (depth - np.min(depth)) / (np.max(depth) - np.min(depth))
-    return normalized_depth
-
-
-def _depth_normalize_2(depth, basepoint='Torso', normalize_factor=150.0):
+def _depth_normalize(depth, basepoint='Pelvis', normalize_factor=151.32):
     idx = keypoint_names.index(basepoint)
     basepoint_depth = depth[idx]
 
@@ -91,9 +85,9 @@ def _interpolate_depth(x, y, x1, y1, z1, x2, y2, z2):
 def _draw_skeleton_2d(img, c, keypoints_2d, normalized_depth):
     w, h = img.size
 
-    # record the minimum depth of each point
-    depth_buffer = np.full((h, w), depth_inf, dtype=np.float32)
-    fused = np.array(img)
+    # depth buffer
+    depth_buffer = np.full((h, w), DEPTH_INF, dtype=np.float32)
+    fused = np.array(img)  # h * w * c, uint8
 
     for t, (i, j) in enumerate(c):
         x1, y1 = keypoints_2d[i]
@@ -107,16 +101,16 @@ def _draw_skeleton_2d(img, c, keypoints_2d, normalized_depth):
         b = int(round(255 * b))
         color = np.array((r, g, b), dtype=np.uint8)
 
+        # draw the line temporarily
         img_line = Image.new('L', (w, h), 'black')  # black:0, white: 255
         draw = ImageDraw.ImageDraw(img_line)
         draw.line((x1, y1, x2, y2), fill='white', width=skeleton_width)
 
+        # filter out the line
         nonzero_y, nonzero_x = np.nonzero(np.array(img_line))  # x: horizontal, v: vertical
         z = _interpolate_depth(nonzero_x, nonzero_y, x1, y1, z1, x2, y2, z2)
 
-        # True: replace, False: leave alone
         mask = np.less(z, depth_buffer[nonzero_y, nonzero_x]).astype(np.float32)
-
         depth_buffer[nonzero_y, nonzero_x] = (1 - mask) * depth_buffer[nonzero_y, nonzero_x] + mask * z
 
         mask_expand = np.expand_dims(mask, -1)
@@ -139,6 +133,15 @@ def _draw_keypoints_2d(img, keypoints_2d):
     return img
 
 
+def _draw_legend():
+    patches = list()
+    for t, (i, j) in enumerate(connections):
+        patch = mpatches.Patch(color=skeleton_colors[t], label='%s_%s' % (keypoint_names[i], keypoint_names[j]))
+        patches.append(patch)
+
+    plt.legend(handles=patches)
+
+
 def skeleton_2d(image_size, keypoints_2d, depth, flip, _DEBUG=False, original_img=None):
     if flip:
         c = connections_flipped
@@ -147,22 +150,26 @@ def skeleton_2d(image_size, keypoints_2d, depth, flip, _DEBUG=False, original_im
 
     img = Image.new('RGB', image_size, color='black')
 
-    normalized_depth = _depth_normalize_1(depth)  # [0, 1]
+    normalized_depth = _depth_normalize(depth)
 
     img = _draw_skeleton_2d(img, c, keypoints_2d, normalized_depth)
     img = _draw_keypoints_2d(img, keypoints_2d)
 
     if _DEBUG:
+        # draw skeleton image
         plt.clf()
         plt.imshow(img)  # skeleton
+        _draw_legend()
         plt.show()
 
+        # draw skeleton over the original image
         original_img2 = original_img.copy()
-        original_img2 = _draw_skeleton_2d(original_img2, c, keypoints_2d, normalized_depth)
-        original_img2 = _draw_keypoints_2d(original_img2, keypoints_2d)
+        img2 = _draw_skeleton_2d(original_img2, c, keypoints_2d, normalized_depth)
+        img2 = _draw_keypoints_2d(img2, keypoints_2d)
 
         plt.clf()
-        plt.imshow(original_img2)  # skeleton drawn over the original image
+        plt.imshow(img2)
+        _draw_legend()
         plt.show()
 
     return img
@@ -175,9 +182,9 @@ def skeleton_rgbd(image_size, keypoints_2d, depth, flip, _DEBUG=False, original_
         c = connections
 
     w, h = image_size
-    depth_img = np.full((h, w), depth_inf, dtype=np.float32)
+    depth_img = np.full((h, w), DEPTH_INF, dtype=np.float32)
 
-    normalized_depth = _depth_normalize_1(depth)
+    normalized_depth = _depth_normalize(depth)
 
     # draw skeleton
     for i, j in c:
@@ -186,19 +193,19 @@ def skeleton_rgbd(image_size, keypoints_2d, depth, flip, _DEBUG=False, original_
         x2, y2 = keypoints_2d[j]
         z2 = normalized_depth[j]
 
+        # draw the line temporarily
         img_line = Image.new('L', image_size, 'black')  # black:0, white: 255
         draw = ImageDraw.ImageDraw(img_line)
         draw.line((x1, y1, x2, y2), fill='white', width=skeleton_width)
 
+        # filter out the line
         nonzero_y, nonzero_x = np.nonzero(np.array(img_line))  # x: horizontal, v: vertical
         z = _interpolate_depth(nonzero_x, nonzero_y, x1, y1, z1, x2, y2, z2)
 
-        depth_img[(nonzero_y, nonzero_x)] = np.minimum(depth_img[(nonzero_y, nonzero_x)], z)
-
-    depth_img[depth_img == depth_inf] = -1.0  # -1.0, [0, 1]
+        depth_img[nonzero_y, nonzero_x] = np.minimum(depth_img[nonzero_y, nonzero_x], z)
 
     # draw circle keypoints
-    for i in range(keypoint_count):
+    for i in np.argsort(-normalized_depth):  # from high to low
         x, y = keypoints_2d[i]
         z = normalized_depth[i]
         radius = float(keypoint_size) / 2
@@ -211,54 +218,29 @@ def skeleton_rgbd(image_size, keypoints_2d, depth, flip, _DEBUG=False, original_
 
         depth_img[nonzero_pos] = z
 
+    depth_img[depth_img == DEPTH_INF] = 0.0
+
     if _DEBUG:
-        colormap = cm.get_cmap(depth_color_map)
-        depth_img2 = colormap(depth_img)  # RGBA
-        depth_img2[depth_img == -1.0] = np.array(colors.to_rgba('black'))
-        depth_img2 = np.round(255.0 * depth_img2).astype(np.uint8)
-
+        # draw depth image
         plt.clf()
-        plt.imshow(Image.fromarray(depth_img2))  # depth image
+        plt.imshow(depth_img, cmap=skeleton_colormap)
+        plt.colorbar()
         plt.show()
 
-        original_img2 = np.array(original_img.convert('RGBA'))
-        nonzero_pos = np.nonzero(depth_img + 1.0)
-        original_img2[nonzero_pos] = depth_img2[nonzero_pos]
-
+        # draw depth over the original image
         plt.clf()
-        plt.imshow(Image.fromarray(original_img2))  # depth image drawn over the original image
+        plt.imshow(original_img, alpha=0.6)
+        plt.imshow(depth_img, cmap=skeleton_colormap, alpha=0.4)
+        plt.colorbar()
         plt.show()
 
-    skeleton_img = skeleton_2d(image_size, keypoints_2d, depth, flip)
+    skeleton_img = skeleton_2d(image_size, keypoints_2d, depth, flip, _DEBUG=_DEBUG, original_img=original_img)
     return skeleton_img, depth_img
 
 
 if __name__ == '__main__':
     # testing
     import pickle
-
-    # draw skeleton color map
-    hist_x = np.arange(connection_count) + 0.5  # 0.5, 1.5, ..., connection_count - 0.5
-    hist_bins = np.arange(connection_count + 1)
-
-    plt.clf()
-    _, _, patches = plt.hist(hist_x, hist_bins)
-    for t, color in enumerate(skeleton_colors):
-        patches[t].set_facecolor(color)
-        i, j = connections[t]
-        patches[t].set_label('%s+%s' % (keypoint_names[i], keypoint_names[j]))
-
-    plt.legend()
-    plt.show()
-
-    # draw depth color map
-    colormap_gradient = np.linspace(0.0, 1.0, 256)
-    colormap_gradient = np.vstack((colormap_gradient, colormap_gradient))
-
-    plt.clf()
-    plt.imshow(colormap_gradient, cmap=cm.get_cmap(depth_color_map))
-    plt.axis('off')
-    plt.show()
 
     image_dir = r'D:\Work\human36m\cropped_images'
     sample_count = 20
@@ -268,29 +250,25 @@ if __name__ == '__main__':
         with open(os.path.join(clip, 'metadata.pkl'), 'rb') as f_in:
             metadata_list = pickle.load(f_in)
 
-        image_count = len(metadata_list)
-        metadata_list.sort(key=lambda x: x[0])
+            image_count = len(metadata_list)
+            metadata_list.sort(key=lambda x: x[0])
 
-        for i in range(sample_count):
-            idx = int(round(float(i) * (image_count - 1) / (sample_count - 1)))
-            file_name, bbox, keypoints_img, depth = metadata_list[idx]
-            keypoints_img = np.round(keypoints_img).astype(np.int64)
+            for i in range(sample_count):
+                idx = int(round(float(i) * (image_count - 1) / (sample_count - 1)))
+                file_name, bbox, keypoints_img, depth = metadata_list[idx]
+                keypoints_img = np.round(keypoints_img).astype(np.int64)
 
-            fullname = os.path.join(clip, file_name)
-            print(fullname)
+                fullname = os.path.join(clip, file_name)
+                print(fullname)
 
-            img = Image.open(fullname).convert('RGB')
-            w, h = img.size
+                img = Image.open(fullname).convert('RGB')
+                w, h = img.size
 
-            # flipped
-            flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
-            flipped_keypoints = keypoints_img.copy()
-            flipped_keypoints[:, 0] = w - 1 - flipped_keypoints[:, 0]
+                skeleton_rgbd((w, h), keypoints_img, depth, False, _DEBUG=True, original_img=img)
 
-            # skeleton_2d
-            skeleton_2d((w, h), keypoints_img, depth, False, _DEBUG=True, original_img=img)
-            skeleton_2d((w, h), flipped_keypoints, depth, True, _DEBUG=True, original_img=flipped_img)
+                # flipped
+                flipped_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                flipped_keypoints = keypoints_img.copy()
+                flipped_keypoints[:, 0] = w - 1 - flipped_keypoints[:, 0]
 
-            # skeleton_rgbd
-            skeleton_rgbd((w, h), keypoints_img, depth, False, _DEBUG=True, original_img=img)
-            skeleton_rgbd((w, h), flipped_keypoints, depth, True, _DEBUG=True, original_img=flipped_img)
+                skeleton_rgbd((w, h), flipped_keypoints, depth, True, _DEBUG=True, original_img=flipped_img)